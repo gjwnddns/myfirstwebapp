0ba8fc3fc724f243616079616e6506e51ef752a5
HurJungUn <HurJungUn>
2024-05-02 02:14:35 +0900

    강좌를 보고 만든 첫 WebApp입니다.

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.myfirstwebapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/resources/").setCachePeriod(20);
+
+	}
+}
